1549370fe475004e26440cf3e4e36ba8dac9cfd9
pottery_stimulation <126481089+swelldisco@users.noreply.github.com>
2024-01-09 10:21:29 -0500

    add UserMapper tests

---
+++ user_service/src/test/java/com/java_coffee/user_service/user/UserMapperTest.java
@@ -0,0 +1,53 @@
+package com.java_coffee.user_service.user;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+@TestInstance(Lifecycle.PER_CLASS)
+public class UserMapperTest {
+    private UserMapper mapper;
+    User testUser;
+    UserDto testUserDto;
+
+    @BeforeAll
+    void setUp() {
+        mapper = new UserMapper();
+        testUser = new User("Billy_Bob", "B-B@gmail.com", "ieaugkfs236432");
+        testUserDto = new UserDto("Billy_Bob", "B-B@gmail.com", "ieaugkfs236432");
+    }
+
+    @AfterAll
+    void tearDown() {
+        mapper = null;
+        testUser = null;
+        testUserDto = null;
+    }
+
+    @Test
+    void testMapToDto() {
+        UserDto dto = mapper.mapToDto(testUser);
+
+        Assertions.assertNotNull(dto.getUserName());
+        Assertions.assertEquals(testUser.getUserName(), dto.getUserName());
+        Assertions.assertNotNull(dto.getEmailAddress());
+        Assertions.assertEquals(testUser.getEmailAddress(), dto.getEmailAddress());
+        Assertions.assertNull(dto.getPassword());
+        Assertions.assertNotEquals(testUser.getPasswordHash(), dto.getPassword());
+    }
+
+    @Test
+    void testMapToUser() {
+        User user = mapper.mapToUser(testUserDto);
+
+        Assertions.assertNotNull(user.getUserName());
+        Assertions.assertEquals(testUserDto.getUserName(), user.getUserName());
+        Assertions.assertNotNull(user.getEmailAddress());
+        Assertions.assertEquals(testUserDto.getEmailAddress(), user.getEmailAddress());
+        Assertions.assertNull(user.getPasswordHash());
+        Assertions.assertNotEquals(testUserDto.getPassword(), user.getPasswordHash());
+    }
+}
