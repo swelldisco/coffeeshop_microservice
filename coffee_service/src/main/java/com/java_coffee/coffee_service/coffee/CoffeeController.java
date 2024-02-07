47cedd1eef2e9be1693df19480c7259eeb3a0321
pottery_stimulation <126481089+swelldisco@users.noreply.github.com>
2024-02-06 19:24:44 -0500

    fixed minor issue with CoffeeController

--- coffee_service/src/main/java/com/java_coffee/coffee_service/coffee/CoffeeController.java
+++ coffee_service/src/main/java/com/java_coffee/coffee_service/coffee/CoffeeController.java
@@ -7,10 +7,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -41,8 +41,8 @@ public class CoffeeController {
     }
 
     // http://127.0.0.1:8082/api_v1/coffee/id?coffeeId=1
-    @GetMapping("/id/{coffeeId}")
-    public ResponseEntity<CoffeeDto> getCoffeeById(@PathVariable long coffeeId) {
+    @GetMapping("/{coffeeId}")
+    public ResponseEntity<CoffeeDto> getCoffeeById(@RequestParam long coffeeId) {
         return new ResponseEntity<>(service.findCoffeeById(coffeeId), HttpStatus.OK);
     }
 
@@ -54,21 +54,21 @@ public class CoffeeController {
 
     // http://127.0.0.1:8082/api_v1/coffee/byName?drinkName=latte
     @GetMapping("/byName/{drinkName}")
-    public ResponseEntity<List<CoffeeDto>> getAllCoffeesByName(@PathVariable String drinkName) {
+    public ResponseEntity<List<CoffeeDto>> getAllCoffeesByName(@RequestParam String drinkName) {
         return new ResponseEntity<>(service.findAllByName(drinkName), HttpStatus.OK);
     }
 
     // http://127.0.0.1:8082/api_v1/coffee/update?coffeeId=1
     @PutMapping("/update/{coffeeId}")
     // also only for admin and barista roles
-    public ResponseEntity<CoffeeDto> updateCoffee(@PathVariable long coffeeId, @Valid CoffeeDto coffeeDto) {
+    public ResponseEntity<CoffeeDto> updateCoffee(@RequestParam long coffeeId, @Valid CoffeeDto coffeeDto) {
         return new ResponseEntity<>(service.updateCoffee(coffeeId, coffeeDto), HttpStatus.ACCEPTED);
     }
 
     // http://127.0.0.1:8082/api_v1/coffee/delete?coffeeId=1
     @DeleteMapping("/delete/{coffeeId}")
     // and again, only for admin and baristas
-    public ResponseEntity<HttpStatus> deleteCoffee(@PathVariable long coffeeId) {
+    public ResponseEntity<HttpStatus> deleteCoffee(@RequestParam long coffeeId) {
         service.deleteCoffeeById(coffeeId);
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
